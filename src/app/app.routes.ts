600573e39761a418ab6fb4edbaddab87eb08095e
Fabio Vital <fabio-trab@outlook.com>
2024-05-25 01:01:46 -0400

    feat: configura rotas do router e cria not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomePageComponent },
+    { path: 'perfil', component: ProfilePageComponent },
+    { path: '**', component: NotFoundPageComponent },
+];
