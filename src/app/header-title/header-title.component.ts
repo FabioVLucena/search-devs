fdeba0c9cc3530955a4385203e6a2a5fa013dfbf
Fabio Vital <fabio-trab@outlook.com>
2024-05-25 02:17:56 -0400

    style: implementa propriedade fontSize e fontWeight ao header-title

--- src/app/header-title/header-title.component.ts
+++ src/app/header-title/header-title.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-title',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-title.component.html',
-  styleUrl: './header-title.component.css'
+  styleUrl: './header-title.component.css',
 })
 export class HeaderTitleComponent {
-
+  @Input() fontSize: number | undefined;
+  @Input() fontWeight: number | undefined;
 }
