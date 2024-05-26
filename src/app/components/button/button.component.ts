7824db88bc99373fed50fd4be7ab1fcb10e7c856
Fabio Vital <fabio-trab@outlook.com>
2024-05-25 23:42:28 -0400

    feat: melhora not-found-page e adiciona botao home

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
-import { Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
 })
 export class ButtonComponent {
+  @Input() label: string = 'Search';
   @Output() submit = new EventEmitter();
-
 }
--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, RouterModule],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.css',
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent {
+  constructor(private router: Router) {}
+
+  goHome(): void {
+    this.router.navigate(['home']);
+  }
+}
